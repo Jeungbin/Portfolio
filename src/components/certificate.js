b9646e8eaf4c96d54b34f0cc30f5c643fe4bacd8
jeongbin <qls2467@gmail.com>
2022-09-25 10:45:41 -0600

    Edit Page Width

---
+++ src/components/certificate.js
@@ -0,0 +1,28 @@
+const certificate = [
+  {
+    id: 1,
+    img: "img/certificate/python.PNG",
+  },
+  {
+    id: 2,
+    img: "img/certificate/html.PNG",
+  },
+  {
+    id: 3,
+    img: "img/certificate/css.PNG",
+  },
+  {
+    id: 4,
+    img: "img/certificate/js.PNG",
+  },
+  {
+    id: 5,
+    img: "img/certificate/jquery.PNG",
+  },
+  {
+    id: 6,
+    img: "img/certificate/asyncJS.PNG",
+  },
+];
+
+export default certificate;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
 import React from "react";
-import Buttons from "../components/Buttons";
 import Works from "../components/Works";
-
 const Home = () => {
   return (
     <main>
-      <Buttons />
       <Works />
     </main>
   );
